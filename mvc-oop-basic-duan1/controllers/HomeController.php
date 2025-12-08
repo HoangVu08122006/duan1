8daa2f1013ece5ca2715fd6b1e68e255a7375307
HoangVu08122006 <tama200006@gmail.com>
2025-12-08 08:34:13 +0700

    Delete mvc-oop-basic-duan1 directory

--- mvc-oop-basic-duan1/controllers/HomeController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function homeIndex() {
-    $homeUrl = 'index.php?act=/';
-
-    // Nếu muốn dùng trong view
-    require __DIR__ . '/../views/home.php';
-}
--- mvc-oop-basic-duan1/models/AdminModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getAdminByUsername($username) {
-    $conn = connectDB();
-    $sql = "SELECT * FROM admin WHERE name_admin = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$username]);
-    return $stmt->fetch();
-}
